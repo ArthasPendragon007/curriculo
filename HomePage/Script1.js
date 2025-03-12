1ad9700335d5e77e5b7e245c408a56a703a474f5
Luiz Arthur Barros De Carvalho <luiz.46424@alunosatc.edu.br>
2025-03-12 19:43:42 -0300

    commit 2

--- HomePage/Script1.js
+++ HomePage/Script1.js
@@ -1,1 +1,5 @@
 // JavaScript source code
+cloudinary.url().transformation(new Transformation()
+    .aspectRatio("1.0").gravity("auto").width(867).crop("fill").chain()
+    .quality("auto").chain()
+    .fetchFormat("auto")).imageTag("hiking_dog_mountain.jpg")
